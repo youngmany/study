c0d23c987cca4bcfb01c6c997bb98cbd587cca19
youngmany <ppp4397@gmail.com>
2019-11-29 19:06:19 +0900

    Add: 1-4: 템플릿을 이용한 메인 페이지 HTML 생성

--- python/tdd_with_python/superlists/functional_test.py
+++ python/tdd_with_python/superlists/functional_test.py
@@ -39,6 +39,7 @@ class NewVisitorTest(unittest.TestCase): #1
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: 공작 깃털 사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않는다" # 사용자 정의 메시지 인수 지정
         )
         
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자 존재
