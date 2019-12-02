3f87fc1bcfc9c5dbb55fc3cd7a60996f91326893
youngmany <ppp4397@gmail.com>
2019-12-02 20:36:37 +0900

    1.5: POST후 리디렉션과 템플릿에 모든 작업 아이템 표시

--- python/tdd_with_python/superlists/lists/tests.py
+++ python/tdd_with_python/superlists/lists/tests.py
@@ -1,3 +1,4 @@
+import re
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -28,6 +29,7 @@ class ItemModelTest(TestCase):
 
 
 class HomePageTest(TestCase):
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
 
     def test_rool_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -43,13 +45,47 @@ class HomePageTest(TestCase):
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
+        request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first() # first == all()[0]
+        self.assertEqual(new_item.text, 'A new list item')
+        """
+        self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
-            {'new_item_text': '신규 작업 아이템'}
+            {'new_item_text': 'A new list item'}
         )
-        # self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html)
+        )
+        """
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
--- python/tdd_with_python/superlists/lists/views.py
+++ python/tdd_with_python/superlists/lists/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Item
 
 def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+    """
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': new_item_text
     })
+    """
