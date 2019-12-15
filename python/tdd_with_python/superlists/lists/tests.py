84e916f821c6ad33a63a79081b796dd4f084be2e
youngmany <ppp4397@gmail.com>
2019-12-15 17:48:10 +0900

    Add: 두 개 폼을 위한 URL 적용,  views, tests 코드 이동

--- python/tdd_with_python/superlists/lists/tests.py
+++ python/tdd_with_python/superlists/lists/tests.py
@@ -42,57 +42,6 @@ class HomePageTest(TestCase):
         # string == string
         # self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first() # first == all()[0]
-        self.assertEqual(new_item.text, 'A new list item')
-        """
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-        self.assertEqual(
-            re.sub(self.pattern_input_csrf, '', response.content.decode()),
-            re.sub(self.pattern_input_csrf, '', expected_html)
-        )
-        """
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
-    """
-    def test_home_page_display_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-        # ListViewTest의 test_display_all_items와 동일한 테스트
-    """
-
 
 class ListViewTest(TestCase):
     
@@ -108,3 +57,25 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
+
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
--- python/tdd_with_python/superlists/superlists/urls.py
+++ python/tdd_with_python/superlists/superlists/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from lists.views import home_page, view_list
+from lists.views import home_page, view_list, new_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
     path('lists/the-only-list-in-the-world/', view_list, name='view_list'),
+    path('lists/new', new_list, name='new_list'),
 ]
